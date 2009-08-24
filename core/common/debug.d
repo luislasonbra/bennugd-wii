be70cbb9dd23b17a2c0f0c10784cb4b6d65f63f6
josebagar <josebagar@bbcc9a24-751d-11de-bcfd-e5a083034d07>
2009-08-24 19:49:58 +0000

    Initial steps of mod_say integration, not yet working, though.

--- core/common/debug.d
+++ core/common/debug.d
@@ -1,88 +1,34 @@
 ../../common/debug.o: ../../common/debug.c \
-  /home/joseba/cvs/wii/bennugd/core/bgdc/../include/pslang.h \
-  /home/joseba/cvs/wii/bennugd/core/bgdc/../include/dcb.h \
-  /home/joseba/cvs/wii/bennugd/core/bgdc/../include/typedef.h \
-  /home/joseba/cvs/wii/bennugd/core/bgdc/../include/typedef_st.h \
-  /home/joseba/cvs/wii/bennugd/core/bgdc/../include/xstrings.h \
-  /home/joseba/cvs/wii/bennugd/core/bgdc/include/bgdc.h \
-  /home/joseba/cvs/wii/bennugd/core/bgdc/include/messages.h \
-  /home/joseba/cvs/wii/bennugd/core/bgdc/../include/commons_defs.h \
-  /home/joseba/cvs/wii/bennugd/core/bgdc/../include/files.h \
-  /home/joseba/cvs/wii/bennugd/core/bgdc/../include/files_st.h \
-  /opt/devkitppc/libogc/include/zlib.h \
-  /opt/devkitppc/libogc/include/zconf.h \
-  /home/joseba/cvs/wii/bennugd/core/bgdc/../include/xctype.h \
-  /home/joseba/cvs/wii/bennugd/core/bgdc/../include/xctype_st.h \
-  /home/joseba/cvs/wii/bennugd/core/bgdc/../include/typedef.h \
-  /home/joseba/cvs/wii/bennugd/core/bgdc/include/constants.h \
-  /home/joseba/cvs/wii/bennugd/core/bgdc/include/identifiers.h \
-  /home/joseba/cvs/wii/bennugd/core/bgdc/../include/xstrings.h \
-  /home/joseba/cvs/wii/bennugd/core/bgdc/../include/arrange.h \
-  /home/joseba/cvs/wii/bennugd/core/bgdc/include/segment.h \
-  /home/joseba/cvs/wii/bennugd/core/bgdc/include/varspace.h \
-  /home/joseba/cvs/wii/bennugd/core/bgdc/include/token.h \
-  /home/joseba/cvs/wii/bennugd/core/bgdc/../include/files.h \
-  /home/joseba/cvs/wii/bennugd/core/bgdc/include/codeblock.h \
-  /home/joseba/cvs/wii/bennugd/core/bgdc/include/procdef.h \
-  /home/joseba/cvs/wii/bennugd/core/bgdc/include/compiler.h \
-  /home/joseba/cvs/wii/bennugd/core/bgdc/../include/dcb.h \
-  /home/joseba/cvs/wii/bennugd/core/bgdc/../include/offsets.h \
-  /home/joseba/cvs/wii/bennugd/core/bgdc/../include/pslang.h
+  /home/joseba/cvs/wii/bennugd-wii/core/bgdrtm/../include/pslang.h \
+  /home/joseba/cvs/wii/bennugd-wii/core/bgdrtm/../include/dcb.h \
+  /home/joseba/cvs/wii/bennugd-wii/core/bgdrtm/../include/typedef.h \
+  /home/joseba/cvs/wii/bennugd-wii/core/bgdrtm/../include/typedef_st.h \
+  /home/joseba/cvs/wii/bennugd-wii/core/bgdrtm/../include/xstrings.h \
+  /home/joseba/cvs/wii/bennugd-wii/core/bgdrtm/include/sysprocs_p.h \
+  /home/joseba/cvs/wii/bennugd-wii/core/bgdrtm/../include/sysprocs_st.h \
+  /home/joseba/cvs/wii/bennugd-wii/core/bgdrtm/../include/instance.h \
+  /home/joseba/cvs/wii/bennugd-wii/core/bgdrtm/../include/instance_st.h \
+  /home/joseba/cvs/wii/bennugd-wii/core/bgdrtm/../include/i_procdef_st.h \
+  /home/joseba/cvs/wii/bennugd-wii/core/bgdrtm/../include/instance_st.h
 
-/home/joseba/cvs/wii/bennugd/core/bgdc/../include/pslang.h:
+/home/joseba/cvs/wii/bennugd-wii/core/bgdrtm/../include/pslang.h:
 
-/home/joseba/cvs/wii/bennugd/core/bgdc/../include/dcb.h:
+/home/joseba/cvs/wii/bennugd-wii/core/bgdrtm/../include/dcb.h:
 
-/home/joseba/cvs/wii/bennugd/core/bgdc/../include/typedef.h:
+/home/joseba/cvs/wii/bennugd-wii/core/bgdrtm/../include/typedef.h:
 
-/home/joseba/cvs/wii/bennugd/core/bgdc/../include/typedef_st.h:
+/home/joseba/cvs/wii/bennugd-wii/core/bgdrtm/../include/typedef_st.h:
 
-/home/joseba/cvs/wii/bennugd/core/bgdc/../include/xstrings.h:
+/home/joseba/cvs/wii/bennugd-wii/core/bgdrtm/../include/xstrings.h:
 
-/home/joseba/cvs/wii/bennugd/core/bgdc/include/bgdc.h:
+/home/joseba/cvs/wii/bennugd-wii/core/bgdrtm/include/sysprocs_p.h:
 
-/home/joseba/cvs/wii/bennugd/core/bgdc/include/messages.h:
+/home/joseba/cvs/wii/bennugd-wii/core/bgdrtm/../include/sysprocs_st.h:
 
-/home/joseba/cvs/wii/bennugd/core/bgdc/../include/commons_defs.h:
+/home/joseba/cvs/wii/bennugd-wii/core/bgdrtm/../include/instance.h:
 
-/home/joseba/cvs/wii/bennugd/core/bgdc/../include/files.h:
+/home/joseba/cvs/wii/bennugd-wii/core/bgdrtm/../include/instance_st.h:
 
-/home/joseba/cvs/wii/bennugd/core/bgdc/../include/files_st.h:
+/home/joseba/cvs/wii/bennugd-wii/core/bgdrtm/../include/i_procdef_st.h:
 
-/opt/devkitppc/libogc/include/zlib.h:
-
-/opt/devkitppc/libogc/include/zconf.h:
-
-/home/joseba/cvs/wii/bennugd/core/bgdc/../include/xctype.h:
-
-/home/joseba/cvs/wii/bennugd/core/bgdc/../include/xctype_st.h:
-
-/home/joseba/cvs/wii/bennugd/core/bgdc/../include/typedef.h:
-
-/home/joseba/cvs/wii/bennugd/core/bgdc/include/constants.h:
-
-/home/joseba/cvs/wii/bennugd/core/bgdc/include/identifiers.h:
-
-/home/joseba/cvs/wii/bennugd/core/bgdc/../include/xstrings.h:
-
-/home/joseba/cvs/wii/bennugd/core/bgdc/../include/arrange.h:
-
-/home/joseba/cvs/wii/bennugd/core/bgdc/include/segment.h:
-
-/home/joseba/cvs/wii/bennugd/core/bgdc/include/varspace.h:
-
-/home/joseba/cvs/wii/bennugd/core/bgdc/include/token.h:
-
-/home/joseba/cvs/wii/bennugd/core/bgdc/../include/files.h:
-
-/home/joseba/cvs/wii/bennugd/core/bgdc/include/codeblock.h:
-
-/home/joseba/cvs/wii/bennugd/core/bgdc/include/procdef.h:
-
-/home/joseba/cvs/wii/bennugd/core/bgdc/include/compiler.h:
-
-/home/joseba/cvs/wii/bennugd/core/bgdc/../include/dcb.h:
-
-/home/joseba/cvs/wii/bennugd/core/bgdc/../include/offsets.h:
-
-/home/joseba/cvs/wii/bennugd/core/bgdc/../include/pslang.h:
+/home/joseba/cvs/wii/bennugd-wii/core/bgdrtm/../include/instance_st.h:
--- core/common/xctype.d
+++ core/common/xctype.d
@@ -1,7 +1,7 @@
 ../../common/xctype.o: ../../common/xctype.c \
-  /home/joseba/cvs/wii/bennugd/core/bgdc/../include/xctype.h \
-  /home/joseba/cvs/wii/bennugd/core/bgdc/../include/xctype_st.h
+  /home/joseba/cvs/wii/bennugd-wii/core/bgdrtm/../include/xctype.h \
+  /home/joseba/cvs/wii/bennugd-wii/core/bgdrtm/../include/xctype_st.h
 
-/home/joseba/cvs/wii/bennugd/core/bgdc/../include/xctype.h:
+/home/joseba/cvs/wii/bennugd-wii/core/bgdrtm/../include/xctype.h:
 
-/home/joseba/cvs/wii/bennugd/core/bgdc/../include/xctype_st.h:
+/home/joseba/cvs/wii/bennugd-wii/core/bgdrtm/../include/xctype_st.h:

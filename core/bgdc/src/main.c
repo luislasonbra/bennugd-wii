a85d015617c2c1e67cdcf24e0fecc363150d7de9
josebagar <josebagar@gmail.com>
2009-09-30 18:55:48 +0000

    * printf is now working in bgdc, I had to initialize the console.   It seems to display nothing yet, though.

--- core/bgdc/src/main.c
+++ core/bgdc/src/main.c
@@ -37,6 +37,7 @@
 
 #ifdef TARGET_WII
 #include <fat.h>
+#include <wiiuse/wpad.h>
 #endif
 
 #include "bgdc.h"
@@ -110,6 +111,43 @@ int main( int argc, char **argv )
 	  printf("Sorry, couldn't go to the root dir on your card :(\n");
 	  return 0;
 	}
+	
+	/* Now initialize the console, needed by printf */
+  static void *xfb = NULL;
+  static GXRModeObj *rmode = NULL;
+
+	// Initialise the video system
+	VIDEO_Init();
+	
+	// This function initialises the attached controllers
+	WPAD_Init();
+	
+	// Obtain the preferred video mode from the system
+	// This will correspond to the settings in the Wii menu
+	rmode = VIDEO_GetPreferredMode(NULL);
+
+	// Allocate memory for the display in the uncached region
+	xfb = MEM_K0_TO_K1(SYS_AllocateFramebuffer(rmode));
+	
+	// Initialise the console, required for printf
+	console_init(xfb,20,20,rmode->fbWidth,rmode->xfbHeight,rmode->fbWidth*VI_DISPLAY_PIX_SZ);
+	
+	// Set up the video registers with the chosen mode
+	VIDEO_Configure(rmode);
+	
+	// Tell the video hardware where our display memory is
+	VIDEO_SetNextFramebuffer(xfb);
+	
+	// Make the display visible
+	VIDEO_SetBlack(FALSE);
+
+	// Flush the video register changes to the hardware
+	VIDEO_Flush();
+
+	// Wait for Video setup to complete
+	VIDEO_WaitVSync();
+	if(rmode->viTVMode&VI_NON_INTERLACE) VIDEO_WaitVSync();
+
 #endif
 
     srand( time( NULL ) );

834cc537f43fb0bafad098bb91b5a8394d2b525e
josebagar <josebagar@gmail.com>
2010-04-24 23:51:24 +0000

    Added iconv header.

---
+++ modules/mod_iconv/mod_iconv.h
@@ -0,0 +1,18 @@
+/* ************************************************************************** */
+/* *                                                                        * */
+/* *            Copyright (c) 2008-2010 - Joseba García Etxebarria          * */
+/* *                                                                        * */
+/* *                   This is a BennuGD interface to iconv                 * */
+/* *                                                                        * */
+/* *            Under the terms of the GNU GPL version 2 license            * */
+/* *                                                                        * */
+/* *                         See LICENSE for details                        * */
+/* *                                                                        * */
+/* ************************************************************************** */
+
+#ifndef __ICONV_H
+#define __ICONV_H
+
+extern int bgd_iconv(INSTANCE * my, int * params);
+
+#endif
